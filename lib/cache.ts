656020e0352dcb247258add9a21c3c7c3b4d7301
arekhalpern <arek.halpern@gmail.com>
2024-12-16 01:41:06 -0500

    update

--- lib/cache.ts
+++ lib/cache.ts
@@ -1,37 +1,17 @@
-import type { Email, Summary } from '@/app/dashboard/types';
+import { Email, EmailThread } from "@/app/dashboard/types";
+import { Summary } from "@/app/dashboard/types";
+import { LRUCache } from "lru-cache";
 
 export interface EmailCacheData {
   email?: Email;
+  thread?: EmailThread;
   summary?: Summary;
   messages?: Email[];
   totalEmails?: number;
   nextPageToken?: string;
 }
 
-export interface EmailCache {
-  get: (key: string) => EmailCacheData | undefined;
-  set: (key: string, value: EmailCacheData) => void;
-  clear: () => void;
-}
-
-class Cache implements EmailCache {
-  private cache: Map<string, EmailCacheData>;
-
-  constructor() {
-    this.cache = new Map();
-  }
-
-  get(key: string) {
-    return this.cache.get(key);
-  }
-
-  set(key: string, value: EmailCacheData) {
-    this.cache.set(key, value);
-  }
-
-  clear() {
-    this.cache.clear();
-  }
-}
-
-export const emailCache = new Cache(); 
+export const emailCache = new LRUCache<string, EmailCacheData>({
+  max: 500, // Maximum number of items
+  ttl: 1000 * 60 * 60, // 1 hour
+}); 
